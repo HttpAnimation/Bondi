b7222680c19e00297ee1c8ab6fde33aa392ecaff
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-10-05 00:38:59 -0700

    Add files via upload

---
+++ V3/Bondi.java
@@ -0,0 +1,119 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bondi extends JFrame {
+
+    private List<String> categories = new ArrayList<>();
+    private JPanel gameButtonPanel;
+    private JPanel subArea;
+
+    public Bondi() {
+        // Load categories from subsections.ini
+        try (BufferedReader reader = new BufferedReader(new FileReader("subsections.ini"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                categories.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        setTitle("Bondi");
+        setUndecorated(true); // Remove window decorations
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize to full screen
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Set the main background color to dark grey
+        getContentPane().setBackground(new Color(245, 154, 175)); // Dark grey color
+
+        setLayout(new BorderLayout());
+
+        // Create a panel for category buttons
+        JPanel categoryPanel = new JPanel(new FlowLayout());
+        categoryPanel.setOpaque(false); // Make the panel transparent
+        for (String category : categories) {
+            JButton categoryButton = new JButton(category);
+            categoryButton.addActionListener(new CategoryButtonListener(category));
+            categoryPanel.add(categoryButton);
+        }
+
+        // Create a sub-area with a white background
+        subArea = new JPanel(new BorderLayout());
+        subArea.setBackground(new Color(207, 140, 156)); // White color
+
+        // Create a panel for game buttons
+        gameButtonPanel = new JPanel(new FlowLayout());
+        gameButtonPanel.setOpaque(false); // Make the panel transparent
+
+        // Add the sub-area to the frame
+        add(categoryPanel, BorderLayout.NORTH);
+        add(subArea, BorderLayout.CENTER); // Add the sub-area to the center
+
+        pack();
+        setLocationRelativeTo(null); // Center the window
+    }
+
+    private class CategoryButtonListener implements ActionListener {
+        private String category;
+
+        public CategoryButtonListener(String category) {
+            this.category = category;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            displayGameButtonsForCategory(category);
+        }
+    }
+
+    private void displayGameButtonsForCategory(String category) {
+        gameButtonPanel.removeAll();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("Games.ini"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                if (parts.length == 3 && parts[0].trim().equals(category.trim())) {
+                    String gameName = parts[1].trim();
+                    JButton gameButton = new JButton(gameName);
+                    String command = parts[2].trim();
+                    gameButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            try {
+                                // Execute the command when the game button is clicked
+                                ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+                                pb.start();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    });
+                    gameButtonPanel.add(gameButton);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        gameButtonPanel.revalidate();
+        gameButtonPanel.repaint();
+
+        // Add gameButtonPanel to the subArea's center
+        subArea.add(gameButtonPanel, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Bondi launcher = new Bondi();
+            launcher.setVisible(true);
+        });
+    }
+}
