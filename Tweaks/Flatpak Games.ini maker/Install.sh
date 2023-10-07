2a337d83e0672bbe2e51c29c0c289114f501dfb7
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-10-07 00:30:33 -0700

    Add files via upload

--- Tweaks/Flatpak Games.ini maker/Install.sh
+++ Tweaks/Flatpak Games.ini maker/Install.sh
@@ -7,5 +7,5 @@ wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/Tweaks/Flatpak%2
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/Tweaks/Flatpak%20Games.ini%20maker/README.md
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/Tweaks/Flatpak%20Games.ini%20maker/subsections.ini
 rm Install.sh # You can comment this out to keep the main Install file / this one
-cd ../
-rm Install.sh 
+echo "To run this tweak run in this terminal window"
+echo "python3 FlatpakGen.py"
