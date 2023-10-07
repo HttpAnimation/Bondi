708592f865892a593bf1af86f5a7dff7b78e1374
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-10-07 00:26:35 -0700

    Add files via upload

--- Tweaks/Flatpak Games.ini maker/Install.sh
+++ Tweaks/Flatpak Games.ini maker/Install.sh
@@ -1,5 +1,5 @@
-mkdir Flatpak Games.ini maker
-cd Flatpak Games.ini maker
+mkdir Flatpak\ Games.ini\ maker
+cd Flatpak\ Games.ini\ maker
 mkdir Output
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/Tweaks/Flatpak%20Games.ini%20maker/FlatpakGen.py
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/Tweaks/Flatpak%20Games.ini%20maker/Games.ini
