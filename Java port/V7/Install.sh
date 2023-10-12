58f97c0ade432959a98a60e6d301fde95e0b949a
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-10-11 21:25:50 -0700

    Add files via upload

---
+++ Java port/V7/Install.sh
@@ -0,0 +1,6 @@
+javac Bondi.java
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/More/V3/RunCommand/Run.sh
+chmod +x Run.sh
+echo "./Run.sh to run the launcher"
+rm Install.sh
+echo "Before the script will work it need to be setup look in the folder Config EXP'S->README.md"
