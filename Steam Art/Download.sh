094c6bbfa4a47b5428a19a7c633bcc2062aa08f4
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-12-10 09:07:06 -0700

    Add files via upload

---
+++ Steam Art/Download.sh
@@ -0,0 +1,8 @@
+mkdir Steam\ Art
+cd Steam\ Art
+wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Logo.jpg?raw=true
+wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Grid.jpg?raw=true
+wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Icon.png?raw=true
+wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Hero.jpg?raw=true
+cd ../
+rm Download.sh
