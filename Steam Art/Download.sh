5f6838770984bf36f18d7096b32c97c08abb7dd6
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-12-10 09:13:41 -0700

    Update Download.sh

--- Steam Art/Download.sh
+++ Steam Art/Download.sh
@@ -1,8 +1,8 @@
-mkdir Steam\ Art
-cd Steam\ Art
-wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Logo.jpg?raw=true
-wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Grid.jpg?raw=true
-wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Icon.png?raw=true
-wget https://github.com/HttpAnimation/Bondi/blob/main/Steam%20Art/Hero.jpg?raw=true
-cd ../
+mkdir -p "Steam Art"
+cd "Steam Art"
+wget -O Logo.jpg https://github.com/HttpAnimation/Bondi/raw/main/Steam%20Art/Logo.jpg
+wget -O Grid.jpg https://github.com/HttpAnimation/Bondi/raw/main/Steam%20Art/Grid.jpg
+wget -O Icon.png https://github.com/HttpAnimation/Bondi/raw/main/Steam%20Art/Icon.png
+wget -O Hero.jpg https://github.com/HttpAnimation/Bondi/raw/main/Steam%20Art/Hero.jpg
+cd ..
 rm Download.sh
