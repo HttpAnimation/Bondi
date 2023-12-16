eafa9d73fdc53baf03980162c71313817bbecb87
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-12-15 21:19:41 -0700

    Add files via upload

--- Installers/V2.bash
+++ Installers/V2.bash
@@ -3,7 +3,9 @@ cd Bondi
 mkdir V2
 cd V2
 wget https://github.com/HttpAnimation/Bondi/raw/main/V2/Bondi.sh
+chmod +x Bondi.sh
 wget https://github.com/HttpAnimation/Bondi/raw/main/V2/Games.ini
 wget https://github.com/HttpAnimation/Bondi/raw/main/V2/subsections.ini
 cd ../
 cd ../
+rm V2.bash
