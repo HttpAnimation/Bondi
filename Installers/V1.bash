e44ecccc51a4530a2550edb27903f29d0de1ea02
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-12-15 21:13:00 -0700

    Add files via upload

---
+++ Installers/V1.bash
@@ -0,0 +1,17 @@
+mkdir Bondi
+cd Bondi
+mkdir V1
+cd V1
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/V1/Bondi.sh
+chmod +x Bondi.sh
+mkdir Config
+cd Config
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/V1/Config/Steam-Path.inf
+mkdir Advance
+cd Advance
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/V1/Config/Advance/Help.html
+cd ../
+cd ../
+cd ../
+cd ../
+rm V1.bash
