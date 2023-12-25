3fd092bb20d35c5341c745c34b45fbf6a5f45389
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-12-25 16:59:36 -0700

    Update Install.sh

--- V8/Install.sh
+++ V8/Install.sh
@@ -1,3 +1,18 @@
+echo "Downloading java files"
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/V8/Bondi.java
+mkdir Configs
+cd Configs
+wget https://github.com/HttpAnimation/Bondi/raw/main/V8/Config/subsections.ini
+wget https://github.com/HttpAnimation/Bondi/raw/main/V8/Config/Update.ini
+wget https://github.com/HttpAnimation/Bondi/raw/main/V8/Config/SidebarWidth.ini
+wget https://github.com/HttpAnimation/Bondi/raw/main/V8/Config/Lightmode.ini
+wget https://github.com/HttpAnimation/Bondi/raw/main/V8/Config/Games.ini
+cd ../
+mkdir "Config EXP'S"
+cd "Config EXP'S"
+wget "https://github.com/HttpAnimation/Bondi/raw/main/V8/Config%20EXP'S/subsections.ini"
+wget "https://github.com/HttpAnimation/Bondi/raw/main/V8/Config%20EXP'S/Games.ini"
+cd ../
 echo "Downloading Build.sh"
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/main/More/Build.sh
 echo "Down downloading Build.sh" 
