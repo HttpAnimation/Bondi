a7c81e3bb1065ed485af52fc4ef4dc2913cb7367
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-04 20:58:47 -0700

    makes buttons load in from the left top

--- Bondi.py
+++ Bondi.py
@@ -59,7 +59,7 @@ class BondiApp:
             name = game["name"]
             command = game["command"]
             button = tk.Button(self.game_buttons_frame, text=name, command=lambda cmd=command: self.run_command(cmd))
-            button.pack(side=tk.TOP)  
+            button.pack(side=tk.LEFT)  # Align buttons to the left along the x-axis
 
     def run_command(self, command):
         os.system(command)
