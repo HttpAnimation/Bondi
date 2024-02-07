212aff0854d2d5ff594761a7aa51cf390195282f
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:17:42 -0700

    adds a cheack for macos and linux

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Check the output of `uname` command
+OS=$(uname)
+
+# Check if the OS is macOS
+if [[ "$OS" == "Darwin" ]]; then
+    echo "This is macOS."
+# Check if the OS is Unix/Linux
+elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
+    echo "This is Unix/Linux."
+else
+    echo "Unknown operating system."
+fi
