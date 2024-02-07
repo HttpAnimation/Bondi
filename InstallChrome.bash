c14c039afac28b8316aaa090321da7b84499b130
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:21:21 -0700

    adds a command to unzip the file for linux

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -7,6 +7,7 @@ elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
     echo "Running on unix/Linux."
     echo "Downloading chromedriver for Linux"
     wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+    unzip chromedriver_linux64.zip
 else
     echo "Unknown operating system."
 fi
