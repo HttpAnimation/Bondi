d3c364fbdc7c5d6d0760c76dff75d8313532a556
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:25:05 -0700

    adds a echo for "Done unziping"

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -8,6 +8,7 @@ elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
     echo "Downloading chromedriver for Linux"
     wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
+    echp "Done unziping"
     echo "Removing the old zip file"
     rm chromedriver_linux64.zip
     echo "Done removing"
