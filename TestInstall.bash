38be13d997e61fee8ea33671c399c05cf2c7390f
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-06 21:35:46 -0700

    forces github to update

--- TestInstall.bash
+++ TestInstall.bash
@@ -5,3 +5,4 @@ cd "$random_dir" || exit
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/BondiWeb.py
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/InstallChrome.bash
 cd "$current_dir" || exit
+ 
