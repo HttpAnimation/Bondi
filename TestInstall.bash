339acb39fd29ecbad524b0fc46892ffe09f24543
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:32:42 -0700

    downloads the gnu LICENSE

--- TestInstall.bash
+++ TestInstall.bash
@@ -3,3 +3,4 @@ random_dir=$(mktemp -d)
 cd "$random_dir" || exit
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/BondiWeb.py
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/InstallChrome.bash
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/LICENSE
