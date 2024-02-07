06d5662a25f399e355fa47721229a13254ed9c37
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:24:03 -0700

    adds a echo to say the script it done removeing the zip

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -9,6 +9,7 @@ elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
     wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
     rm chromedriver_linux64.zip
+    echo "Done removing"
     echo "Done installing chomedriver"
 else
     echo "Unknown operating system."
