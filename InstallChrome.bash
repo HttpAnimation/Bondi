a5802c38cc6da72a5125b0b7a89d2ac03ba81ace
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:17:52 -0700

    remvoes # Check if the OS is Unix/Linux

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -6,7 +6,6 @@ OS=$(uname)
 # Check if the OS is macOS
 if [[ "$OS" == "Darwin" ]]; then
     echo "This is macOS."
-# Check if the OS is Unix/Linux
 elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
     echo "This is Unix/Linux."
 else
