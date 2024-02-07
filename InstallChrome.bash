56a421941007362ad9e71b3cce5b4e2bfcb514fe
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:20:32 -0700

    adds the wget command to download chrome backend from google

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -6,6 +6,7 @@ if [[ "$OS" == "Darwin" ]]; then
 elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
     echo "Running on unix/Linux."
     echo "Downloading chromedriver for Linux"
+    wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 else
     echo "Unknown operating system."
 fi
