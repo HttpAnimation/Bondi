5accfd837f04fbef3ccd2b040d60b55aaa517d37
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:34:57 -0700

    Fixs cd

--- TestInstall.bash
+++ TestInstall.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
-random_dir=$(mktemp -d)
+current_dir=$(pwd)
+random_dir=$(mktemp -d "$current_dir/tmp.XXXXXX")
 cd "$random_dir" || exit
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/BondiWeb.py
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/InstallChrome.bash
-wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/LICENSE
+cd "$current_dir" || exit
