6d6b0fd1a9c30e49363f220cc300950603abefae
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-06 21:19:23 -0700

    adds Running on unix/Linux

--- InstallChrome.bash
+++ InstallChrome.bash
@@ -4,7 +4,7 @@ OS=$(uname)
 if [[ "$OS" == "Darwin" ]]; then
     echo "This is macOS."
 elif [[ "$OS" == "Linux" || "$OS" == "GNU" ]]; then
-    echo "This is Unix/Linux."
+    echo "Running on unix/Linux."
 else
     echo "Unknown operating system."
 fi
