13f0c7ecb32129d602067556d116b949e6250ab5
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-09 11:18:31 -0500

    adds a message saying what it is downloading

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ echo "Bondi installer"
 echo "Lsat updated: Friday/9/Feb/2024"
 echo "Fuck anti furs, homophobes and racist"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo "Downloading the Json editor."
 wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/JsonEditorApp.py
