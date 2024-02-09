1cde0a6215fd5e1096f7fd13d2ddc59599fb811b
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-09 11:17:09 -0500

    removes the git clone

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@ echo "Bondi installer"
 echo "Lsat updated: Friday/9/Feb/2024"
 echo "Fuck anti furs, homophobes and racist"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
-git clone -b stable https://github.com/HttpAnimation/Bondi.git
