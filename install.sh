a383e72840c3bfc82b1324035438f07442a98eb0
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-09 11:17:38 -0500

    adds a wget fro the json editer

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ echo "Bondi installer"
 echo "Lsat updated: Friday/9/Feb/2024"
 echo "Fuck anti furs, homophobes and racist"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+wget https://raw.githubusercontent.com/HttpAnimation/Bondi/stable/JsonEditorApp.py
