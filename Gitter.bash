50aa1116f97aba367e85dfd8743b8f0165483138
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-16 20:56:31 -0700

    Create Gitter.bash

---
+++ Gitter.bash
@@ -0,0 +1,10 @@
+git clone -b stable https://github.com/HttpAnimation/Bondi.git
+mv Bondi stable
+git clone -b main https://github.com/HttpAnimation/Bondi.git
+mv Bondi main 
+git clone -b gh-pages https://github.com/HttpAnimation/Bondi.git
+mv Bondi gh-pages
+git clone -b blank https://github.com/HttpAnimation/Bondi.git
+mv Bondi blank
+git clone -b Repo-Testing https://github.com/HttpAnimation/Bondi.git
+mv Bondi Repo-Testing
