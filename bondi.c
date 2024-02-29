6b3960b1e9d39e7ccc2794c8f23e24faba674944
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-29 12:21:02 -0500

    Bondi

--- bondi.c
+++ bondi.c
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) {
 
     // Create main window
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Bondi - Game/App Launcher");
+    gtk_window_set_title(GTK_WINDOW(window), "Bondi");
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
