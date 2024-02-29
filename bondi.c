3d9c83d9b55aef69df402c98a8bdb9d7f549e571
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-29 12:22:09 -0500

    adds version 3

--- bondi.c
+++ bondi.c
@@ -116,7 +116,7 @@ int main(int argc, char *argv[]) {
 
     // Create main window
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Bondi");
+    gtk_window_set_title(GTK_WINDOW(window), "Bondi - V3");
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
