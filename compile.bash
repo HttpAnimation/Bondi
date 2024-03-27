c4e6335ffbb099facb889fcb1ddbe18c9338770b
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-26 18:35:00 -0700

    system info

--- compile.bash
+++ compile.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 clear
 
+# Detect the operating system
+if [[ "$(uname)" == "Darwin" ]]; then
+    OS="macOS"
+elif [[ "$(uname)" == "Linux" ]]; then
+    OS="Linux"
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
 # Get current date and time
 current_datetime=$(date +'%Y-%m-%d_%H-%M-%S')
 
@@ -9,12 +19,14 @@ build_folder="$current_datetime"
 mkdir -p "$build_folder"
 
 # Compile c scripts into the build folder
-# gcc -o "$build_folder/openGLTest" openGLTest.c -lglfw -lGL -I/usr/include
-gcc -o "$build_folder/bondiLinux" bondiLinux.c `pkg-config --cflags --libs gtk+-3.0`
-gcc -o "$build_folder/bondiMac" bondiMac.c `pkg-config --cflags --libs gtk+-3.0`
+if [[ "$OS" == "macOS" ]]; then
+    gcc -o "$build_folder/bondiMac" bondiMac.c `pkg-config --cflags --libs gtk+-3.0`
+elif [[ "$OS" == "Linux" ]]; then
+    gcc -o "$build_folder/bondiLinux" bondiLinux.c `pkg-config --cflags --libs gtk+-3.0`
+fi
 
 # Copy files into the build folder
-cp openGLTest.c "$build_folder"
+cp bondi*.c "$build_folder" # Assuming your source files start with bondi
 
 # Create a Version.ini file with the current date and time
 echo "$current_datetime" > "$build_folder/Version.ini"
@@ -28,5 +40,8 @@ echo "This is an auto-generated message and might work or might not." >> "$readm
 # Print the path to the build folder
 echo "Builds created at: $build_folder"
 echo "You can also run it by typing this in:"
-echo "./$build_folder/bondiLinux"
-echo "./$build_folder/bondiMac"
+if [[ "$OS" == "macOS" ]]; then
+    echo "./$build_folder/bondiMac"
+elif [[ "$OS" == "Linux" ]]; then
+    echo "./$build_folder/bondiLinux"
+fi
