f199e0717c37a80647010b4e40b70fe7c477aef8
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-26 18:32:43 -0700

    Bondi Linux

--- compile.bash
+++ compile.bash
@@ -30,4 +30,4 @@ echo "This is an auto-generated message and might work or might not." >> "$readm
 # Print the path to the build folder
 echo "Builds created at: $build_folder"
 echo "You can also run it by typing this in:"
-echo "./$build_folder/test"
+echo "./$build_folder/bondi"
